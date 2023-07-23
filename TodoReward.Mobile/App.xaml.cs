6b9d5ba00d109055cc93477cc77e5e7427399deb
Mattias Morell <dev@mattiasmorell.se>
2023-07-23 11:06:34 +0200

    Dark theme by default

--- TodoReward.Mobile/App.xaml.cs
+++ TodoReward.Mobile/App.xaml.cs
@@ -6,6 +6,8 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+        Application.Current.UserAppTheme = AppTheme.Dark;
+
+        MainPage = new AppShell();
 	}
 }
